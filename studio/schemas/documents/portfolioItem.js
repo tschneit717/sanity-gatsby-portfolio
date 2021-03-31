57ec61b192f82aa3d969e3a8db3883a6da4edff8
tschneit717 <tschneit717@gmail.com>
2021-03-31 14:01:13 -0400

    Rename project items

---
+++ studio/schemas/documents/portfolioItem.js
@@ -0,0 +1,52 @@
+import { format } from "date-fns";
+
+export default {
+  name: "portfolioItem",
+  title: "Portfolio Item",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string"
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96
+      }
+    },
+    {
+      name: "excerpt",
+      title: "Excerpt",
+      type: "simplePortableText"
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "figure"
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "projectPortableText"
+    }
+  ],
+  preview: {
+    select: {
+      title: "title",
+      publishedAt: "publishedAt",
+      slug: "slug",
+      media: "mainImage"
+    },
+    prepare({ title = "No title", media }) {
+      return {
+        title,
+        media
+      };
+    }
+  }
+};
