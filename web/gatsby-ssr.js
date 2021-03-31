829b0bb584a43c956aa245898a1cf6e7cfeb2f8a
tschneit717 <tschneit717@gmail.com>
2021-03-31 11:08:29 -0400

    Updated build process so it stops failing

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -1,22 +1,6 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react";
+import Layout from "./src/components/Layout";
 
-// You can delete this file if you're not using it
-
-const React = require("react");
-const Layout = require("./src/components/layout");
-
-// Adds a class name to the body element
-exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
-  setBodyAttributes({
-    className: "my-body-class",
-  });
-};
-
-// Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
+export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
-};
+}
