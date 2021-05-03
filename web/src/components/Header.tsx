256cf76bd85f6b7906dce670ad2a2ac5f7ede7de
tschneit717 <tschneit717@gmail.com>
2021-05-03 16:56:57 -0400

    Pulled styles out of header component

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,29 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { ContainerStyles } from '../assets/styles/GlobalStyles';
-
-const HeaderStyles = styled.div`
-  > .hero-wrapper {
-    padding: 10px;
-    min-height: 400px;
-    display: flex;
-    justify-content: center;
-    align-items: left;
-    flex-direction: column;
-  }
-  h2 {
-    font-size: 50px;
-  }
-  p {
-    font-size: 24px;
-  }
-  h2,
-  p {
-    color: white;
-    margin: 0;
-    padding: 0;
-  }
-`;
+import React from "react";
+import { HeaderStyles } from "./../assets/styles/components/HeaderStyles";
+import { ContainerStyles } from "../assets/styles/GlobalStyles";
 
 export default function Header({ mainText, subText, color }) {
   return (
